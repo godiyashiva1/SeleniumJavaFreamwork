7a7a412f4ffd71468265777abbe747eb1d91ee1b
Harshad <Harshad@192.168.0.9>
2023-03-20 09:08:26 -0400

    1st build

---
+++ testArtifactID/src/test/java/testArtifactID/TestNG1.java
@@ -0,0 +1,12 @@
+package testArtifactID;
+
+import org.testng.annotations.Test;
+
+public class TestNG1 {
+
+	@Test
+	public void test1() {
+		System.out.println("Hello Selenium");
+		System.out.println("Hello Java");
+	}
+}
